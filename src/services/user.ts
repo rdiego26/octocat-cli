504fbb1970384dbb3090fb7d41ea32c74c849bd8
ramos <rdiego26@gmail.com>
2024-11-30 18:35:33 +0000

    feat: add service layer

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,14 +1,27 @@
+import db from '../config/db';
 import { insertUser, insertLanguage } from '../db/queries';
 import { fetchGitHubUser, fetchUserRepos } from '../config/github';
+import { cleanAndUnique } from "../utils";
 
-export const saveUserData = async (username: string) => {
-    const user = await fetchGitHubUser(username);
-    const { id: userId } = await insertUser(user.name, user.location);
+export const saveUserData = async (username: string): Promise<string> => {
+    try {
+        return await db.tx(async (tx) => {
+            const user = await fetchGitHubUser(username);
+            const languages = await fetchUserRepos(username);
+            const { id: userId } = await insertUser(tx, user.name, user.location, user.avatar_url);
 
-    const languages = await fetchUserRepos(username);
-    await Promise.all(
-        languages.map((lang: string) => insertLanguage(userId, lang))
-    );
+            const sanitizedLanguages = cleanAndUnique(languages);
 
-    return userId;
+            await Promise.all(
+                sanitizedLanguages
+                    .map((lang: string) => insertLanguage(tx, userId, lang))
+            );
+
+            return userId;
+        });
+    } catch (error: any) {
+        console.error('Error during transaction:', error.message);
+        throw new Error('Failed to save user data');
+    }
+
 };
