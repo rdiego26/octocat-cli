e954e4bf07fbfd55a31fd510b1be864ad0c6f451
ramos <rdiego26@gmail.com>
2024-11-30 18:53:06 +0000

    chore: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{ts}"]},
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "max-len": ["error", { code: 80 }],
+      "indent": ["error", 2, { SwitchCase: 1 }],
+      "no-tabs": "error",
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "@typescript-eslint/explicit-function-return-type": "off",
+    },
+  }
+];
